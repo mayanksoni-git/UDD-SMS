d9eeb8989dde97d3c975c922d83a7c81a04f47e8
Mayank Soni <Mayank Soni@LAPTOP-5I290M2A>
2024-10-19 12:27:30 +0530

    Rsolve Error of Vision Plan

--- Scheme_Monitoring/App_Code/ULBFund.cs
+++ Scheme_Monitoring/App_Code/ULBFund.cs
@@ -89,24 +89,24 @@ public class ULBFund
         }
     }
 
-    public DataTable GetParkAdoptionReport(string actions, int dist, int fY, int month)
-    {
-        try
-        {
-            DataTable dt = new DataTable();
-            SqlParameter[] param = new SqlParameter[4];
-            param[0] = new SqlParameter("@action", actions);
-            param[1] = new SqlParameter("@DistrictId", dist);
-            param[2] = new SqlParameter("@FYId", fY);
-            param[3] = new SqlParameter("@MonthId", month);
+    //public DataTable GetParkAdoptionReport(string actions, int dist, int fY, int month)
+    //{
+    //    try
+    //    {
+    //        DataTable dt = new DataTable();
+    //        SqlParameter[] param = new SqlParameter[4];
+    //        param[0] = new SqlParameter("@action", actions);
+    //        param[1] = new SqlParameter("@DistrictId", dist);
+    //        param[2] = new SqlParameter("@FYId", fY);
+    //        param[3] = new SqlParameter("@MonthId", month);
 
-            return objDAL.GetDataByProcedure("Sp_GetParkAdoptionReport", param);
-        }
-        catch (Exception ex)
-        {
-            throw new Exception(ex.Message);
-        }
-    }
+    //        return objDAL.GetDataByProcedure("Sp_GetParkAdoptionReport", param);
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        throw new Exception(ex.Message);
+    //    }
+    //}
 
 
     #region ULB Income
@@ -1062,5 +1062,24 @@ INNER JOIN tbl_ULBIncomeType ex on a.HeadID=ex.ULBIncomeType_Id
         }
     }
 
+
+    public DataTable GetParkAdoptionReport(string actions, int dist, int fY, int month)
+    {
+        try
+        {
+            DataTable dt = new DataTable();
+            SqlParameter[] param = new SqlParameter[4];
+            param[0] = new SqlParameter("@action", actions);
+            param[1] = new SqlParameter("@DistrictId", dist);
+            param[2] = new SqlParameter("@FYId", fY);
+            param[3] = new SqlParameter("@MonthId", month);
+
+            return objDAL.GetDataByProcedure("Sp_GetParkAdoptionReport", param);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
 
 }
